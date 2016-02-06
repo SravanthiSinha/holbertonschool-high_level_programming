0dd68ba18d3f7224b6b548a36248b8e49f90aeb5
Sravanthi Rani Sinha <37@p6-2.hbtn.io>
2016-02-06 11:08:24 -0800

    Improved DocumentationI

--- discover_python/taxes_and_tip_calculator.py
+++ discover_python/taxes_and_tip_calculator.py
@@ -5,9 +5,10 @@ Author:Sravanthi Sinha
 Date:03-02-2016
 """
 
+#Display of welcome message
 print 'Welcome to the taxes and tip calculator!'
 
-#input variables
+#Collecting user inputs 
 meal_price = float(raw_input("What is the price before tax?"))
 tax = float(raw_input("What are the taxes? (in %)"))
 tip = float(raw_input("What do you want to tip? (in %) "))
@@ -19,7 +20,7 @@ meal_price=meal_price+meal_price*tax*0.01
 tip=meal_price*tip*0.01
 
 
-#calculating total meal price with tip and tax included.
+#calculating the total meal price with tip and tax included.
 total=meal_price+tip
 
 print 'The price you need to pay is: %f.'%(total)
