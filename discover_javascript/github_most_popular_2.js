c9c726063e0e0a326ac6398769e963968d2490dc
Sravanthi Sinha <sravani.sinha@gmail.com>
2016-02-15 11:00:00 -0800

    using token from environmental variables.

--- discover_javascript/github_most_popular_2.js
+++ discover_javascript/github_most_popular_2.js
@@ -4,9 +4,9 @@ var options = {
     host: 'api.github.com',
     path: '/search/repositories?q=language:javascript&sort=stars&order=desc',
     headers: {
-	'User-Agent': 'Holberton_School',
-	'Authorization': 'token 627a195aa92718ab4e6b284389c8b581bc5e531d'
-    }
+    'User-Agent': 'Holberton_School',
+    'Authorization': 'token ' + process.env.TOKEN
+  }
 };
 
 var req = https.request(options, function(res) {
