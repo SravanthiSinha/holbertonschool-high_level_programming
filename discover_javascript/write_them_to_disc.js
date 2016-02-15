6f80cc5d787275ea082a637fae5bb34abc45a3eb
Sravanthi Sinha <sravani.sinha@gmail.com>
2016-02-15 11:01:14 -0800

    using token from environmental variables.

--- discover_javascript/write_them_to_disc.js
+++ discover_javascript/write_them_to_disc.js
@@ -4,10 +4,10 @@ var fs=require('fs');
 var options = {
     host: 'api.github.com',
     path: '/search/repositories?q=language:javascript&sort=stars&order=desc',
-    headers: {
-	'User-Agent': 'Holberton_School',
-	'Authorization': 'token 627a195aa92718ab4e6b284389c8b581bc5e531d'
-    }
+     headers: {
+    'User-Agent': 'Holberton_School',
+    'Authorization': 'token ' + process.env.TOKEN
+  }
 };
 
 var req = https.request(options, function(res) {
